7e78352dd0c15239eb3e425fbefd6789ff770507
menna_tarek <mennatallah.dawood@gmail.com>
2015-04-12 12:36:00 +0200

    Add Exchange #GET #POST

--- application/controllers/Exchange.php
+++ application/controllers/Exchange.php
@@ -39,6 +39,93 @@ class Exchange extends REST_Controller{
         $this->response($data);
 
     }
+
+    function exchange_get()
+    {
+        $world = $this->db->get('Country');
+        foreach ($world->result() as $country) 
+        {
+            $this -> db -> select('crop_id');
+            $this -> db -> from('Trade');
+            $this -> db -> where('country_id', $country->id);
+            $this -> db -> where('type',1);
+
+            $imported_crops = $this -> db -> get() -> result_array();
+
+            foreach ($imported_crops as $imported_crop) 
+            {
+
+                $this -> db -> select('country_id');
+                $this -> db -> from('Trade');
+                $this -> db -> where('crop_id', $imported_crop['crop_id']);
+                $this -> db -> where('type',0);
+
+                $exporting_countries = $this -> db -> get() -> result_array();
+                foreach ($exporting_countries as $exporting_country)
+                {
+                    $this -> db -> select('crop_id');
+                    $this -> db -> from('Trade');
+                    $this -> db -> where('country_id', $exporting_country['country_id']);
+                    $this -> db -> where('type',1);
+                    
+                    $exchange_crops = $this -> db -> get() -> result_array();
+
+                    foreach ($exchange_crops as $exchange_crop) 
+                    {
+                        $this -> db -> select('*');
+                        $this -> db -> from('Trade');
+                        $this -> db -> where('crop_id', $exchange_crop['crop_id']);
+                        $this -> db -> where('country_id', $country->id);
+                        $this -> db -> where('type',0);
+
+                        $useful_export = $this -> db -> get() -> result_array();
+
+                        if ($useful_export != null) 
+                        {
+                            $this -> db -> select('amount');
+                            $this -> db -> from('Trade');
+                            $this -> db -> where('country_id', $country->id);
+                            $this -> db -> where('crop_id', $imported_crop['crop_id']);
+                            $this -> db -> where('type',1);
+
+                            $my_import_amount = $this -> db -> get() -> result_array()[0];
+
+                            $this -> db -> select('amount');
+                            $this -> db -> from('Trade');
+                            $this -> db -> where('country_id', $country->id);
+                            $this -> db -> where('crop_id', $exchange_crop['crop_id']);
+                            $this -> db -> where('type',0);
+
+                            $my_export_amount = $this -> db -> get() -> result_array()[0];
+
+                            $this -> db -> select('amount');
+                            $this -> db -> from('Trade');
+                            $this -> db -> where('country_id', $exporting_country['country_id']);
+                            $this -> db -> where('crop_id', $imported_crop['crop_id']);
+                            $this -> db -> where('type',0);
+
+                            $their_import_amount = $this -> db -> get() -> result_array()[0];
+
+
+                            $this -> db -> select('amount');
+                            $this -> db -> from('Trade');
+                            $this -> db -> where('country_id', $exporting_country['country_id']);
+                            $this -> db -> where('crop_id', $exchange_crop['crop_id']);
+                            $this -> db -> where('type',1);
+
+                            $their_export_amount = $this -> db -> get() -> result_array()[0];
+
+                            $amount = min($my_import_amount,$my_export_amount,$their_import_amount,$their_export_amount);
+                            $this -> Exchange_model ->insert_entry($exporting_country['country_id'],$country->id,$imported_crop['crop_id'],$amount);
+                            $this -> Exchange_model ->insert_entry($country->id,$exporting_country['country_id'],$exchange_crop['crop_id'],$amount);
+
+                        }
+
+                    }
+                }
+            }
+        }  
+    }
 }
 
 ?>
