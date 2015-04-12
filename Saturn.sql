b7bc6ee7e4af7168940de8676713b7863e56f582
menna_tarek <mennatallah.dawood@gmail.com>
2015-04-12 03:14:45 +0200

    Add Crop and User Model and Controller

--- Saturn.sql
+++ Saturn.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 11, 2015 at 06:26 PM
+-- Generation Time: Apr 11, 2015 at 07:05 PM
 -- Server version: 5.5.41-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.7
 
@@ -132,7 +132,8 @@ CREATE TABLE IF NOT EXISTS `User` (
   `username` varchar(200) COLLATE utf8_bin NOT NULL,
   `email` varchar(200) COLLATE utf8_bin NOT NULL,
   `password` varchar(100) COLLATE utf8_bin NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
