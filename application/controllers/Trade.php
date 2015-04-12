c500449ce2cd5003158477ba6eaa821ef9cfcc4e
Ahmed Shaher <a.shaher16@gmail.com>
2015-04-12 04:01:49 +0200

    Trade model and Controller

---
+++ application/controllers/Trade.php
@@ -0,0 +1,44 @@
+<?php
+require APPPATH.'/libraries/REST_Controller.php';
+
+class Trade extends REST_Controller{
+
+    function __construct()
+    {
+        // Call the Model constructor
+        parent::__construct();
+        $this->load->model('Trade_model');
+        $this->load->model('Country_model');
+        $this->load->model('Crop_model');
+
+    }
+    function trades_get(){
+        $data = $this->Trade_model->getAllEntries();
+        $this->response($data);
+    }
+    function trade_post(){
+        $supplier_id = $this->post('supplier_id');
+        $receiver_id = $this->post('receiver_id');
+        $crop_id = $this->post('crop_id');
+        $amount = $this->post('amount');
+
+
+        $supplier = $this->Country_model->get_country($supplier_id);
+        $receiver= $this->Country_model->get_country($receiver_id);
+        $crop= $this->Crop_model->get_crop($crop_id);
+
+        $data = new stdClass();
+
+        $data->supplier_id = $supplier->id;
+        $data->receiver_id = $receiver->id;
+        $data->crop_id = $crop->id;
+        $data->amount = $amount;
+
+        $this->db->insert("Trade", $data);
+
+        $this->response($data);
+
+    }
+}
+
+?>
---
+++ application/models/Trade_model.php
@@ -0,0 +1,31 @@
+<?php
+
+class Trade_model extends CI_Model {
+
+    function __construct()
+    {
+        // Call the Model constructor
+        parent::__construct();
+    }
+    
+    function getAllEntries()
+    {
+        $query = $this->db->get("Trade");
+        return $query->result();
+    }
+
+    function insert_entry($supplier_id , $reciever_id , $crop_id , $amount)
+    {
+        $this->country_id = $supplier_id;
+        $this->crop_id = $reciever_id;
+        $this->value = $crop_id;
+        $this->value = $amount;
+        $this->db->insert("Trade", $this);
+    }
+    function get_entry($id){
+        $query = $this->db->select("*")->where("id",$id)->from("Trade");
+        return $query->result();
+    }
+
+}
+?>
